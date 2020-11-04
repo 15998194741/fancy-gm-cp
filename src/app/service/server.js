e42351ba99638939e9aa060b78a2a293426958b6
lixindong <lixindong@fancyguo.cn>
2020-11-04 13:47:07 +0800

    serverFindAll

--- src/app/service/server.js
+++ src/app/service/server.js
@@ -11,12 +11,16 @@ class serverService{
 		let { gameName } = data;
 		let { platform } = data;
 		let { channelNum } = data;
+		let { versionId } = data;
 		let  a = channelNum && platform && gameName;
 		if(!a){throw {message:'缺少参数'};}
 		let sql = `
 		with qwe as ( select id from gm_game  where game_name = '${gameName}' and status = 1 ),
-				asd as (select channel  from gm_game_channel,qwe where  channel_id = '${channelNum}' and gameid  =  qwe.id and status = 1  )
-				select a.* ,'${channelNum}' as channelNum from gm_server a,asd  where  plaform @> '"${platform}"' and  a.channel @> concat('["' ,asd.channel,'"]' )::jsonb and status = 1  `;
+				asd as (select channel  from gm_game_channel,qwe where  channel_id = '${channelNum}' and gameid  =  qwe.id and status = 1  ),
+				zxc as *(select a.* ,'${channelNum}' as channelNum from gm_server a,asd  where  plaform @> '"${platform}"' and  a.channel @> concat('["' ,asd.channel,'"]' )::jsonb and status = 1 ),
+				ert as  (select case when type = '测试' then '1' when type ='正式' then '0' end  as test from (select jsonb_array_elements_text(is_show_type) as type from gm_client,qwe where game_id::int = qwe.id  and version_id =  '${versionId}') a )
+				select * from zxc where zxc.test in (select * from ert ) 
+				 `;
 		let res = await dbSequelize.query(sql, {
 			replacements:['active'], type:Sequelize.QueryTypes.SELECT
 		});
