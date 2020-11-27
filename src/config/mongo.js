fde618b017537e184396032261f9167b3c4a44e4
lixindong <lixindong@fancyguo.cn>
2020-11-27 11:13:45 +0800

    pgsql

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -6,7 +6,8 @@ let MongoDbAction = {};
 // let tabConf = JSON.parse(fs.readFileSync(path.normalize(filename)));
 var tabConf;
 const mongoose = require('./mongo-config.js');
-var conStr = 'postgres://postgres:Ll789456@123.57.86.38:5432/xiaolu';
+// var conStr = 'postgres://postgres:Ll789456@123.57.86.38:5432/xiaolu';
+var conStr = 'postgres://postgres:Ll789456@127.0.0.1:5432/xiaolu';
 var pgOpt = require('pg');
 var client = new pgOpt.Client(conStr);
 // console.log(tabConf);
@@ -34,7 +35,7 @@ async function connectPgWithoutPool() {
 }
 
 function addJsons(name, datas) {
-	var conStr = 'postgres://postgres:Ll789456@123.57.86.38:5432/xiaolu';
+	var conStr = 'postgres://postgres:Ll789456@127.0.0.1:5432/xiaolu';
 	var pgOpt = require('pg');
 	var client = new pgOpt.Client(conStr);
 	client.connect(function (isErr) {
